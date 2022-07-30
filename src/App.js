6f854c14c715a96b874028c86eab37093289c8f0
Aminjon <vafokulov552@gmail.com>
2022-07-30 22:32:48 +0500

    first app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import React, {
+  useState,
+  useEffect
+} from 'react'
 import './App.css';
 
+
 function App() {
+  const [fake, setFake] = useState([])
+  console.log(fake);
+  useEffect(() => {
+    ReactApp()
+  }, [])
+
+  const ReactApp = async () => {
+    const res = await fetch("http://localhost:3001/users")
+    const UserData = await res.json()
+    setFake(UserData)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "countainer">
+      {fake.map((item) => {
+        return(
+          <div className='box'>
+            <div className="box_top">
+                <img className='img' src={item.image} alt="" />
+            </div>
+            <div className="box_bottom">
+                <span className='big_text'>{item.model}</span>
+                <span className='decription'>{item.description}</span>
+            </div>
+          </div>
+        )
+      })}
     </div>
   );
 }
